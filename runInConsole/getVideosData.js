fdce8891226b09566fffdf5f0a9ee69d06674308
Jirinrin <jiri.ei@hotmail.com>
2021-05-10 13:15:51 +0200

    Improve getVideosData

--- runInConsole/getVideosData.js
+++ runInConsole/getVideosData.js
@@ -1,8 +1,10 @@
 var items = document.querySelector('#items.ytd-grid-renderer')
-var nodes = items.childNodes
+var nodes = items.querySelectorAll('ytd-grid-video-renderer')
 var data = [...nodes].map(n => ({
   title: n.querySelector('#video-title').innerText,
   thumb: n.querySelector('#img').src,
   link: n.querySelector('a#thumbnail').href,
 })).reverse() // old to new
 var dataString = JSON.stringify(data, null, 2);
+
+// console.log(dataString);
