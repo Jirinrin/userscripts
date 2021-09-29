210f69d88ebe365c1fa53dcfc1c6beeb0eb13ac9
Jirinrin <jiri.ei@hotmail.com>
2021-09-29 21:16:22 +0200

    00ps

--- googleLyricsEnters.user.js
+++ googleLyricsEnters.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         BandcampEmailDownload
+// @name         GoogleLyricsEnter
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Puts extra enters after paragraphs in lyrics preview in Google
