fd183ff7ce61837150921dea8f4b5cbe1e1448c1
Jirinrin <jiri.ei@hotmail.com>
2021-11-25 21:51:44 +0100

    Add linkcore lyrics enter script

---
+++ linkcoreLyricsEnters.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         GoogleLyricsEnter
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Formats linkcore lyrics to be easier to copy-paste
+// @author       You
+// @match        https://linkco.re/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  var lyricsParent = document.querySelector('.lyric_text');
+  var children = lyricsParent?.querySelectorAll('p');
+
+  if (!children) {
+    return;
+  }
+  console.log('Found lyrics');
+
+  const groups = [...children].reduce((acc, c) => {
+    console.log(c.innerHTML);
+    if (c.innerHTML)
+      acc[acc.length-1].push(c);
+    else {
+      acc.push([]);
+    }
+
+    lyricsParent.removeChild(c);
+
+    return acc;
+  }, [[]]);
+  console.log('groups', groups)
+
+  groups.forEach(g => {
+    const p = document.createElement('p');
+    lyricsParent.appendChild(p);
+    g.forEach(c => {
+      const sp = document.createElement('span');
+      sp.innerHTML = c.innerHTML + '<br>';
+      return p.appendChild(sp);
+    });
+  });
+})();
